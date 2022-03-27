cdbb04810f7bf1fdd69f62bcd398221a7e16a17e
ramirezandradesara <sararamirezandrade@gmail.com>
2022-03-27 00:19:18 -0300

    handleClick done

--- src/App.js
+++ src/App.js
@@ -6,42 +6,83 @@ import data from './data.json';
 
 
 class App extends React.Component {
-constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-      contador: 1,
+      contador: 0,
       anteriorEleccion: "",
-      historial: ["A", "B", "A", "A"],
-     
+      historial: [],
+      contadorElecciones: 0
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-handleClick = (e) => {
-  const eleccion = e.target.value;
-  const historial = this.state.historial;
-  historial.push(eleccion);
-  this.setState({
-    contador: this.state.contador + 1,
-    anteriorEleccion: eleccion,
-    historial: historial,
-  });
-}
+/* componentDidUpdate() {
+  if (this.state.contadorElecciones === 4){
+    alert("Fin")
+  }
+} */
 
+
+  handleClick = (e) => {
+    
+    if (this.state.contadorElecciones === 4) {
+      alert("Fin")
+      
+    } else if (e.target.id === "a" && this.state.anteriorEleccion === "a") { //
+      this.setState({
+        contador: this.state.contador + 2,
+        anteriorEleccion: "a",
+        historial: [...this.state.historial, "a"],
+        contadorElecciones: this.state.contadorElecciones + 1
+      });
+
+    } else if (e.target.id === "b" && this.state.anteriorEleccion !== "a") {
+      this.setState({
+        contador: this.state.contador + 2,
+        anteriorEleccion: "b",
+        historial: [...this.state.historial, "b"],
+        contadorElecciones: this.state.contadorElecciones + 1
+      });
+
+    } else if (e.target.id === "b" && this.state.anteriorEleccion === "a") {//
+      this.setState({
+        contador: this.state.contador + 3,
+        anteriorEleccion: "b",
+        historial: [...this.state.historial, "b"],
+        contadorElecciones: this.state.contadorElecciones + 1
+      });
+
+    } else if (e.target.id === "a" && this.state.anteriorEleccion !== "a") { //
+      this.setState({
+        contador: this.state.contador + 1,
+        anteriorEleccion: "a",
+        historial: [...this.state.historial, "a"],
+        contadorElecciones: this.state.contadorElecciones + 1
+      });
+    }
+  }
 
   render() {
+
+    console.log(this.state.contadorElecciones);
     return (
       <div className='layout'>
-        <Historia 
-        historia={data[this.state.contador].historia}
+        <Historia
+          historia={data[this.state.contador].historia}
         />
-        <Opciones 
-        opcionA={data[0].opciones.a}
-        opcionB={data[0].opciones.b}
+        <Opciones
+          opcionA={data[this.state.contador].opciones.a}
+          opcionB={data[this.state.contador].opciones.b}
+          onClick={this.handleClick}
         />
-        <Recordatorio 
-        anteriorEleccion={this.state.anteriorEleccion}
-        historial={this.state.historial.map(eleccion => <li>{eleccion}</li>)}
-        
+        <Recordatorio
+          anteriorEleccion={this.state.anteriorEleccion}
+          historial={this.state.historial.map((eleccion, index) =>
+            <li key={index}>
+              {eleccion}
+            </li>)}
+
         />
       </div>
 
--- src/Opciones.jsx
+++ src/Opciones.jsx
@@ -2,20 +2,34 @@ import React from 'react'
 import { Component } from 'react'
 
 class Opciones extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+    
+        }
+        this.onClick = this.onClick.bind(this)
+    }
+
+    onClick(e) {
+        this.props.onClick(e.target.id)
+        console.log(e.target.id);
+    }
+
     render() {
+       
         return (
             <div className='opciones'>
-                <div id="a" className="opcion">
-                    <button className='botones'>
+                <div className="opcion">
+                    <button id="a" className='botones' onClick={this.props.onClick}>
                         A
                     </button>
                     <h2>{this.props.opcionA}</h2>
                 </div>
-                <div id="b" className="opcion">
-                    <button className='botones'>
+                <div className="opcion">
+                    <button id="b" className='botones' onClick={this.props.onClick }>
                         B
                     </button>
-                    <h2>{this.props.opcionB }</h2>
+                    <h2>{this.props.opcionB}</h2>
                 </div>
 
             </div>
